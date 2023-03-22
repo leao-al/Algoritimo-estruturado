0aaf3defb61a3522bd3bbc568f76ab8a00ee880d
Arthur Leao <arthurleao.af@gmail.com>
2023-03-21 21:03:46 -0300

    Lista 1 exercicio 1

---
+++ Lista 1_01.c
@@ -0,0 +1,18 @@
+// Arthur Afonso Fernandes Leao
+
+
+/*seu primeiro programa em qualquer linguagem de programação normalmente é
+o "Hello World!". Neste primeiro problema tudo o que você precisa fazer é imprimir
+esta mensagem na tela.*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main (){
+
+printf("Hello word");
+
+
+    return 0;
+}
