19fa026fb1fed6f8aa869f018c5bf43a4ac4486c
Arthur Leao <arthurleao.af@gmail.com>
2023-03-21 21:05:09 -0300

    Lista1_02.c

---
+++ Lista1_02.c
@@ -0,0 +1,20 @@
+// Arthur Afonso Fernandes leão
+
+/*Faça um algoritmo que imprima o poema abaixo, com a mesma formatação:
+Caderno de poesias é um belo lugar.
+Tantas coisas lindas que eu gostaria de falar.
+Eu falo em forma de versos para todos poderem escutar.
+Agora você já sabe por que os poetas passam os dias escrevendo em seus
+cadernos de poesias*/
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main (){
+
+printf("Caderno de poesisas e um belo lugar.\t\n Tantas coisas lindas que eu gostaria de falar.\t\nEu falo em forma de versos para todos poderem escutar.\t\n Agora voce ja sabe por que os poetas passam os dias escrevendo em seus cadernos de poesia.");
+
+    return 0;
+  }
