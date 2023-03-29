dc386e21a8d32086964e85233034c964279b919d
Arthur Leao <116287671+leao-al@users.noreply.github.com>
2023-03-28 21:38:43 -0300

    Add files via upload

--- string2.c
+++ string2.c
@@ -10,17 +10,20 @@ int resposta;
 
 printf("Digite o nome:");
 scanf("%s", &nome);
+fgets(nome, 100, stdin);
 fflush(stdin);
 
 printf("Digite o sobrenome:");
 scanf("%s", &sobrenome);
+fgets(sobrenome, 100, stdin); // substitui o scanf, o "100" e a quantidade de caracteres lidos, o stdin serve como fflush, limpando a memoria.
 fflush(stdin);
 
 if(strlen(nome) == strlen(sobrenome))
     printf("iguais");
     else if (strlen(nome) > strlen(sobrenome))
         printf("nome e maior: ");
-        else printf("nome e menor ");
+        else 
+        printf("nome e menor ");
 
 
 
