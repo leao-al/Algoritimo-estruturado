0036dc607a255891ab7cfa970d90ef858b5156cd
Arthur Leao <116287671+leao-al@users.noreply.github.com>
2023-03-28 21:40:08 -0300

    Add files via upload

--- string2.c
+++ string2.c
@@ -15,7 +15,7 @@ fflush(stdin);
 
 printf("Digite o sobrenome:");
 scanf("%s", &sobrenome);
-fgets(sobrenome, 100, stdin); // substitui o scanf, o "100" e a quantidade de caracteres lidos, o stdin serve como fflush, limpando a memoria.
+fgets(sobrenome, 100, stdin); //fgets conta os espaços!!  substitui o scanf para ler a variavel, o "100" e a quantidade de caracteres lidos, o stdin serve como fflush, limpando a memoria.
 fflush(stdin);
 
 if(strlen(nome) == strlen(sobrenome))
